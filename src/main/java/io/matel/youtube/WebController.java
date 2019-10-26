c5175808fafde85a32b020f2af1e3803e78e6f5c
Mathieu Matel <matel222@hotmail.com>
2019-10-26 08:20:24 +0700

    initial commit

--- src/main/java/io/matel/youtube/WebController.java
+++ src/main/java/io/matel/youtube/WebController.java
@@ -1,13 +1,18 @@
 package io.matel.youtube;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
 
+    @Value("${api.key}")
+    private String apiKey;
+
     @GetMapping("hello")
     public String sayHello(){
+        System.out.println(apiKey);
         return "Hello!";
     }
 }
--- src/main/java/io/matel/youtube/YoutubeApplication.java
+++ src/main/java/io/matel/youtube/YoutubeApplication.java
@@ -2,8 +2,14 @@ package io.matel.youtube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:credentials.properties")
+})
 public class YoutubeApplication {
 
     public static void main(String[] args) {
